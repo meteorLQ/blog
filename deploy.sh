b408d5f4d9a72c09e503353fc3250a9493340570
shadowLQ <46966686+meteorLQ@users.noreply.github.com>
2023-01-27 23:27:20 +0800

    build:更换脚本文字

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/shadowLQ/blog.git master:pro
+git push -f https://github.com/meteorLQ/blog master:pro
 
 cd -
